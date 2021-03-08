5fe91cf2e0d4a045d8c29fc0f8fef26598a511a6
77011-61-04 <77011-61-04@exchange.ad.afpanet>
2021-03-08 16:31:46 +0100

    Finishing version 1

---
+++ tests/FlashMcQueenTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class FlashMcQueenTest extends TestCase
+{
+
+    public function testError()
+    {
+
+    }
+
+    public function testInfo()
+    {
+
+    }
+
+    public function testWarning()
+    {
+
+    }
+
+    public function testSuccess()
+    {
+
+    }
+
+    public function testCustom()
+    {
+
+    }
+
+    public function testDisplay()
+    {
+
+    }
+}
